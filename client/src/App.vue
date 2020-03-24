3b9cebbb9a784edb871f355295669fbf53f86a39
Bob Bass <bob@bbass.co>
2020-03-24 01:46:42 -0400

    created framework for debtor portal

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <Navbar />
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
 
 <style>
 #app {
